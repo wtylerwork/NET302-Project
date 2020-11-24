084a02f9270c54caa0a7d4ef6a63256102e3f862
William Tyler <will.tyler.work@gmail.com>
2020-11-24 14:08:36 +0000

    Initial Web Files
    24/11/2020

---
+++ HomePage.php
@@ -0,0 +1,48 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<!-- Required Metadata -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<!-- Import Materialize Framework -->
+		<link rel="stylesheet" href="css/materialize.min.css">
+		<!-- Import Google Icons -->
+		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+		<!-- Import Custom Styles -->
+		<link rel="stylesheet" href="css/styles.css">
+
+		<title>UK Weather</title>
+
+	</head>
+	<body>
+<header>
+</header>
+<footer class="page-footer" style="background-color: #2C3539">
+	<div class="container">
+		<div class="row">
+			<div class="col l6 s12">
+				<h5 class="white-text">Jericho Defence Services Inc.</h5>
+				<p class="grey-text text-lighten-4">"Making your day a brighter place &trade;" since 2020.</p>
+			</div>
+			<div class="col l4 offset-l2 s12">
+				<h5 class="white-text">Links</h5>
+				<ul>
+					<li><a class="grey-text text-lighten-3" href="https://www.jdsi.com/store/" target="_blank">Weather Application</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="footer-copyright">
+		<div class="container">
+			© 2020 William Tyler (NET302_Project)
+		</div>
+	</div>
+</footer>
+	</body>
+</html>
